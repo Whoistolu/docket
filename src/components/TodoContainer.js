96603da88d32d3455269072a2b33837b6b9732e5
Whoistolu <digittolu25@gmail.com>
2021-11-16 13:28:43 +0100

    Add new state to todocontainer.js

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,14 +1,36 @@
 import React from "react";
+import TodosList from "./TodosList";
+import Header from "./Header"
 
 
 class TodoContainer extends React.Component {
-  render() {
-    return(
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+   };
+
+   render() {
+    return (
       <div>
-        <h1>Hello guys!</h1>
-        <p>I am building something beautiful</p>
+        <Header />
+        <TodosList todos={this.state.todos} />
       </div>
-    )
+    );
   }
 }
 
